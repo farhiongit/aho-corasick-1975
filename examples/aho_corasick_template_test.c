a613a58e71e262b144e8b2fdcf9c2e2289ef0000
Laurent Farhi <lrspam@sfr.fr>
2018-08-05 22:46:24 +0200

    Added machine printer (mostly for debugging purpose)

--- examples/aho_corasick_template_test.c
+++ examples/aho_corasick_template_test.c
@@ -95,7 +95,8 @@ print_match (MatchHolder (wchar_t) match, void *value)
 }
 
 // Must return the number of shifts to the right (that is the number of printed characters)
-int print_wchar_t (FILE *f, const wchar_t wc)
+static int
+print_wchar_t (FILE *f, const wchar_t wc)
 {
   return fprintf (f, "%lc", wc);
 }
